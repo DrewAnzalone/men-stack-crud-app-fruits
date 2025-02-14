52904467395853933f0cba3848701b7d9725bf13
Drew-A <drew.a.anzalone@gmail.com>
2025-02-14 10:25:53 -0800

    added form and DB uploading

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const URI = process.env.MONGODB_URI;
 
 const app = express();
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+
 
 mongoose.connect(URI);
 mongoose.connection.on("connected", () => print(`Connected to MongoDB ${mongoose.connection.name}.`));
@@ -16,6 +18,18 @@ mongoose.connection.on("connected", () => print(`Connected to MongoDB ${mongoose
 // GET /
 app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+
+// GET /fruits/new
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+  req.body.isReadyToEat = req.body.isReadyToEat === "on";
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
 });
 
 app.listen(3000, () => print('Listening on port 3000'));
