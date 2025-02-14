7992d6a6e07ecd50903d5903657d39a5c4d7a5d4
Drew-A <drew.a.anzalone@gmail.com>
2025-02-14 14:02:51 -0800

    added edit + delete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const methodOverride = require("method-override");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,6 +10,7 @@ const URI = process.env.MONGODB_URI;
 
 const app = express();
 app.use(morgan("dev"));
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: false }));
 
 
@@ -27,22 +29,41 @@ app.get("/fruits", async (req, res) => {
   res.render("fruits/index.ejs", { fruits: allFruits });
 });
 
+// GET /fruits/new
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+// GET /fruits/id/edit
+app.get("/fruits/:id/edit", async (req, res) => {
+  const fruit = await Fruit.findById(req.params.id);
+  res.render("fruits/edit.ejs", { fruit });
+});
+
 // GET /fruits/id
 app.get("/fruits/:id", async (req, res) => {
   const fruit = await Fruit.findById(req.params.id);
   res.render("fruits/show.ejs", { fruit });
 });
 
-// GET /fruits/new
-app.get("/fruits/new", (req, res) => {
-  res.render("fruits/new.ejs");
-});
-
 // POST /fruits
 app.post("/fruits", async (req, res) => {
   req.body.isReadyToEat = req.body.isReadyToEat === "on";
   await Fruit.create(req.body);
+  res.redirect("fruits");
+});
+
+// DELETE /fruits/id
+app.delete("/fruits/:id", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.id);
   res.redirect("/fruits");
 });
+
+// PUT /fruits/id
+app.put("/fruits/:id", async (req, res) => {
+  req.body.isReadyToEat = req.body.isReadyToEat === "on";
+  await Fruit.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect(`/fruits/${req.params.id}`);
+})
 
 app.listen(3000, () => print('Listening on port 3000'));
